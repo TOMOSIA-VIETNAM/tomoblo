9b8f0f2cff7cfcc3e7cc0b3345ca12810ff0795b
Anh Nguyen <miyamoto.anh@gmail.com>
2020-06-22 12:48:40 +0700

    Updates

---
+++ component---src-pages-404-js-755d19313b6360790c02.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,t,n){"use strict";n.r(t);var l=n("q1tI"),r=n.n(l),a=n("Bl7J"),u=n("vrFN");t.default=function(){return r.a.createElement(a.a,null,r.a.createElement(u.a,{title:"404: Not found"}),r.a.createElement("h1",null,"Page Not Found"),r.a.createElement("p",null,r.a.createElement("i",null,"Just lightin' out for the territories, huh? Brother, I been there.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-755d19313b6360790c02.js.map
