bf857b0607c34c8dd13028d0a3382e5a15ea4ac5
Anh Nguyen <miyamoto.anh@gmail.com>
2020-06-25 10:53:52 +0700

    Gitalk

--- src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography";
-const typography = new Typography({
-  baseFontSize: "18px",
-  baseLineHeight: 1.666,
-  headerFontFamily: ["Lato", "Helvetica Neue", "Helvetica", "sans-serif"],
-  bodyFontFamily: ["Lato", "Helvetica Neue", "Helvetica", "sans-serif"]
-});
-export default typography;
