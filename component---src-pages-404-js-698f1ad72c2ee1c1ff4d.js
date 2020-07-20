7bea6c7ea8ba0f859d79c75286ca814979d7ea9d
Nguyen Anh <anh.nguyen1@tomosia.com>
2020-07-20 02:09:51 +0000

    Updates

---
+++ component---src-pages-404-js-698f1ad72c2ee1c1ff4d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,t,n){"use strict";n.r(t);var l=n("q1tI"),r=n.n(l),a=n("Bl7J"),u=n("vrFN");t.default=function(){return r.a.createElement(a.a,null,r.a.createElement(u.a,{title:"404: Not found"}),r.a.createElement("h1",null,"Page Not Found"),r.a.createElement("p",null,r.a.createElement("i",null,"Just lightin' out for the territories, huh? Brother, I been there.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-698f1ad72c2ee1c1ff4d.js.map
